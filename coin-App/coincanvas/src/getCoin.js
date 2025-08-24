399233eee6da94c6acfe66e548b70bc7dcc3d468
ImrenR <imrenrahbay@gmail.com>
2025-08-24 19:31:03 +0100

    update

--- coin-App/coincanvas/src/getCoin.js
+++ coin-App/coincanvas/src/getCoin.js
@@ -1,10 +1,28 @@
 // we will work here with modules, the function we call it in main.js ( getCoin()) means: "Hey! take this function and use it outside"
 //! Important note : whenever we click to submit our value the result will come "works!" therefore we need to call input as parameter inside getCoin() ==> main.js!
 
-const getCoin = (query) => {
+// export const getCoin = (query) => {
+//   console.log("works !", query); // this will work when we import this module file otherwise does not work !
+
+//   const url = `https://api.coinranking.com/v2/coins?search=${query}`; // I defined the API in this URL as a variable ${query}
+
+//   const options = {
+//     headers: {
+//       "x-access-token":
+//         "coinranking80243c50e232ab1eeddadb0935e226d1e9661848afaed4c2", //we got the second value from API key usage- cointranking website-generate API
+//     },
+//   };
+ 
+//   fetch(url, options) // change the link with our url
+//     .then((response) => response.json())
+//     // .then((result) => console.log(result.data.coins[2]));
+//     .then((result) => console.log(result))
+// }; 
+
+export const getCoin = async(query) => {
   console.log("works !", query); // this will work when we import this module file otherwise does not work !
 
-  const url = `https://api.coinranking.com/v2/coins?search=${query}`; // I defined the API in this URL as a variable ${query}
+  const url =`https://api.coinranking.com/v2/coins?search=${query}`; // I defined the API in this URL as a variable ${query}
 
   const options = {
     headers: {
@@ -12,10 +30,13 @@ const getCoin = (query) => {
         "coinranking80243c50e232ab1eeddadb0935e226d1e9661848afaed4c2", //we got the second value from API key usage- cointranking website-generate API
     },
   };
- 
-  fetch(url, options) // change the link with our url
-    .then((response) => response.json())
-    .then((result) => console.log(result.data.coins[2]));
+ try { // catch the error
+  const res = await fetch(url,options) // we need to catch response
+  const coinData = await res.json() // dont forget await, keyword await need to be use all the time since it s async 
+  console.log(coinData)
+ } catch (error) {
+  console.error("there is an error")
+ }
 }; 
 
-export default getCoin; // since we opened this function outside need to export it !
+
