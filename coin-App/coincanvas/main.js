94c11cc8f77df3e4a9c97eb1e33623263f4ca8f7
ImrenR <imrenrahbay@gmail.com>
2025-08-24 18:40:46 +0100

    reset form

--- coin-App/coincanvas/main.js
+++ coin-App/coincanvas/main.js
@@ -4,16 +4,17 @@ import "./scss/style.scss"; // we add sass package to make connet sass codes aut
 
 const form = document.querySelector("header form"); //we can use form instead input since form submits as well.
 // console.log(form)
-const input = document.querySelector("header form input");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("form triggered"); //after we used preventDefault we prevent the form from submitting
+  // console.log("form triggered"); //after we used preventDefault we prevent form from submitting
 
   getInputVal(); // we call a function to able to run input and enter our values into it.
+  form.reset(); // to clean the data inside the form
 });
 
 const getInputVal = () => {
   // console.log(input.value) // when we click on "search" the button triggered and showed the value we entered on console (dont forget we added .value)
-  
+  const input = document.querySelector("header form input").value ;
+   console.log(input)
 };
