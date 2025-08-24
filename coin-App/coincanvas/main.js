17cc20ec32b9c28e488e9b2642ae6b3e73c7b3a2
ImrenR <imrenrahbay@gmail.com>
2025-08-24 18:32:53 +0100

    notes

--- coin-App/coincanvas/main.js
+++ coin-App/coincanvas/main.js
@@ -1,18 +1,19 @@
-import "./scss/style.scss";
+import "./scss/style.scss"; // we add sass package to make connet sass codes automaticly, this one of benefits of vite and node.js
 
 // console.log("olaaaaa")
 
-const form = document.querySelector("header form");
+const form = document.querySelector("header form"); //we can use form instead input since form submits as well.
 // console.log(form)
+const input = document.querySelector("header form input");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("form triggered"); //after we used preventDefault we prevent the form from submitting
 
-  getInputVal();
+  getInputVal(); // we call a function to able to run input and enter our values into it.
 });
 
 const getInputVal = () => {
-  const input = document.querySelector("header form input");
-  // console.log(input) // when we click on "search" the button triggered
+  // console.log(input.value) // when we click on "search" the button triggered and showed the value we entered on console (dont forget we added .value)
+  
 };
