79cbd4a35f19f770eb458dee4f99bd5ffa44778c
ImrenR <imrenrahbay@gmail.com>
2025-08-24 18:04:50 +0100

    js file

--- coin-App/coincanvas/main.js
+++ coin-App/coincanvas/main.js
@@ -1,3 +1,18 @@
-import "./scss/style.scss"
+import "./scss/style.scss";
 
 // console.log("olaaaaa")
+
+const form = document.querySelector("header form");
+// console.log(form)
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("form triggered"); //after we used preventDefault we prevent the form from submitting
+
+  getInputVal();
+});
+
+const getInputVal = () => {
+  const input = document.querySelector("header form input");
+  // console.log(input) // when we click on "search" the button triggered
+};
